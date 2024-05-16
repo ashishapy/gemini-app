70180e189b4c934583663876464ab4dbde60871c
Ashish Pandey <ashishapy@gmail.com>
2024-05-16 00:06:52 -0400

    initial Vertex AI Gemini Project

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+import streamlit as st
+from app_tab1 import rendor_story_tab
+from vertexai.preview.generative_models import GenerativeModel
+import vertexai
+import logging
+from google.cloud import logging as google_logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Attach a Google Cloud Logging handler to the root logger
+log_client = google_logging.Client()
+log_client.setup_logging()
+
+PROJECT_ID = os.getenv('PROJECT_ID')
+LOLCATION = os.getenv('REGION')
+vertexai.init(project=PROJECT_ID, location=LOLCATION)
+
+@st.cache_resource  # This will cache the data across runs
+def load_models():
+    text_model_pro = GenerativeModel('gemini-pro')
+    multimodal_model_pro = GenerativeModel('gemini-pro-vision')
+    return text_model_pro, multimodal_model_pro
+
+st.header("Vertex AI Gemini AI", divider="rainbow")
+text_model_pro, multimodal_model_pro = load_models()
+
+tab1, tab2, tab3, tab4 = st.tabs(["Story", "Marketing Campaign", "Image Playground", "Video Playground"])
+
+with tab1:
+    rendor_story_tab(text_model_pro)
